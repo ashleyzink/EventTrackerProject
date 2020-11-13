80ff83515ca67f4fcb0c7825e4ee713c9bffeee4
Ashley Zink <pamperedzink@gmail.com>
2020-11-13 16:11:05 -0700

    Validate JPA/Hibertnate config

--- JPACookie/src/main/java/com/skilldistillery/cookies/entities/Cookie.java
+++ JPACookie/src/main/java/com/skilldistillery/cookies/entities/Cookie.java
@@ -1,5 +1,72 @@
 package com.skilldistillery.cookies.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cookie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+
+	public Cookie() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Cookie [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cookie other = (Cookie) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 
 }
--- JPACookie/src/test/java/com/skilldistillery/cookies/entities/CookieTest.java
+++ JPACookie/src/test/java/com/skilldistillery/cookies/entities/CookieTest.java
@@ -2,33 +2,50 @@ package com.skilldistillery.cookies.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CookieTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Cookie cookie;
+	
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CookiePU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		cookie = em.find(Cookie.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		cookie = null;
 	}
 
 	@Test
+	@DisplayName("test Cookie entity mapping")
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(cookie);
+		assertEquals("Oatmeal Scotchies", cookie.getName());
 	}
 
 }
