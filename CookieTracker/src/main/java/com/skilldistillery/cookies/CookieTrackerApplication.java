497bd1dcc339a2869cc022dda57bd05cfe385315
Ashley Zink <pamperedzink@gmail.com>
2020-11-13 16:41:58 -0700

    Stub Boot project with JPA Project

---
+++ CookieTracker/src/main/java/com/skilldistillery/cookies/CookieTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieTrackerApplication.class, args);
+	}
+
+}
