896c482915a9e59e52a4b81ed0781163dc85b40b
Ashley Zink <pamperedzink@gmail.com>
2020-11-20 18:01:42 -0700

    Added .js and index.html files

--- CookieTracker/src/main/java/com/skilldistillery/cookies/CookieTrackerApplication.java
+++ CookieTracker/src/main/java/com/skilldistillery/cookies/CookieTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.cookies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CookieTrackerApplication {
+public class CookieTrackerApplication extends SpringBootServletInitializer {
+
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(CookieTrackerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CookieTrackerApplication.class, args);
---
+++ CookieTracker/src/main/resources/static/js/eventTracker.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+	console.log('Script loaded');
+	
+	init();
+});
+
+function init() {
+	console.log('in init() still');
+	// TODO: set up event listeners for buttons etc.
+}
